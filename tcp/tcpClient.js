30fcf61924f10a2cb34be41da6ef5c6a74f0d3e5
Gabriel Vanderlei <gabrielvanderlei10@hotmail.com>
2022-07-14 23:29:11 +0000

    add buffer version of the return

---
+++ tcp/tcpClient.js
@@ -0,0 +1,90 @@
+const {
+    DEBUG,
+    PORT,
+    HOST,
+    clientLog,
+    Marshaller 
+} = require('../lib/configuration');
+
+try {
+    const netLib = require('net');
+    const client = new netLib.Socket();
+
+    const readline = require('readline');
+    const rl = readline.createInterface(process.stdin, process.stdout);
+
+    let processMessage = (command, message) => {
+        clientLog(`Command: ${command} / Message: ${message}`, {
+            isDebugOnly: true
+        });
+
+        if (command == 'quit') {
+            clientLog('Bye');
+            client.end();
+            rl.close();
+        } else {
+            let numbers = message.split(' ');
+            let n1 = numbers[0];
+            let n2 = numbers[1];
+
+            client.write(Marshaller({ command, n1, n2}));
+        }
+    }
+
+    const startProcessing = () => {
+        clientLog(`Message format: [command] content.`)
+        clientLog(``)
+        clientLog(`Command examples.`)
+        clientLog(`> sum 1 1`)
+        clientLog(`> + 1 1`)
+        clientLog(`> subtraction 1 1`)
+        clientLog(`> - 1 1`)
+        clientLog(`> division 1 1`)
+        clientLog(`> / 1 1`)
+        clientLog(`> multiplication 1 1`)
+        clientLog(`> * 1 1`)
+        clientLog(`> quit now`)
+        clientLog(``)
+
+        rl.on('line', (message) => {
+            let messageSplitted = message.split(' ');
+
+            if (messageSplitted.length > 1) {
+                let command = messageSplitted.shift();
+                let message = messageSplitted.join(' ');
+
+                processMessage(command, message);
+            } else {
+                clientLog(`Message format: [command] content. Example: message hi`)
+            }
+        });
+    }
+
+    client.connect({
+        port: PORT,
+        host: HOST
+    }, function () {
+        clientLog(`Connected!`);
+
+        rl.question('What is your username? ', (username) => {
+            processMessage('name', username)
+            startProcessing();
+
+            client.on('data', function (chunk) {
+                console.log(`[SERVER] ${chunk.toString()}.`);
+            });
+        });
+    });
+
+    client.on('end', function () {
+        clientLog('Disconnected');
+        client.end();
+        rl.close();
+    });
+
+    client.on('error', function () {
+        clientLog('Error, please verify if the server is online.');
+    });
+} catch (e) {
+    console.log("Error running the client, please verify if the server is online.")
+}
---
+++ tcp/tcpServer.js
@@ -0,0 +1,93 @@
+const {
+    PORT,
+    HOST,
+    serverLog,
+    UnMarshaller ,
+    getRandomId
+} = require('../lib/configuration');
+
+const {
+    processOperation,
+    isOperation
+} = require('../lib/operations');
+
+try {
+    const netLib = require('net');
+    const server = new netLib.Server();
+
+    let allSockets = [];
+
+    let processMessage = ({
+        command,
+        message
+    }, socket) => {
+        if (isOperation(command)) {
+            const {
+                finalResult,
+                allInputsValid
+            } = processOperation(command, message);
+
+            if (allInputsValid) {
+                socket.write(`[${String(command).toUpperCase()}]: ${finalResult}`);
+            } else {
+                socket.write(`[${String(command).toUpperCase()}]: Invalid parameters, please only send numbers`);
+            }
+        }
+
+        if (command == 'name') {
+            socket.name = message;
+            serverLog(`[ACTION] ${socket.id} has the username ${message}`, {
+                isDebugOnly: true
+            });
+        }
+    }
+
+    server.listen(PORT, function () {
+        serverLog(`Server on ${HOST}:${PORT}`);
+    });
+
+    server.on('connection', function (socket) {
+        let id = getRandomId();
+        serverLog(`New client!`, {
+            isDebugOnly: true
+        });
+
+        socket.id = id;
+        socket.write('You are connected');
+
+        allSockets.push(socket);
+
+        socket.on('data', function (chunk) {
+            let receivedInfo = JSON.stringify(chunk.toJSON());
+
+            serverLog(`Data received: ${receivedInfo}`, {
+                isDebugOnly: true
+            });
+
+            try {
+                const messageObject =  UnMarshaller(chunk);
+                
+                const messageFormat = {
+                    command: messageObject.command,
+                    message: `${messageObject.n1} ${messageObject.n2}`
+                };
+
+                processMessage(messageFormat, socket)
+            } catch (e) {
+                serverLog(`Error reading object: ${JSON.stringify(e)}`);
+            }
+        });
+
+        socket.on('end', function () {
+            serverLog(`Bye, ${socket.name} see you`, {
+                isDebugOnly: true
+            });
+        });
+
+        socket.on('error', function (err) {
+            serverLog(`Error: ${err}`);
+        });
+    });
+} catch (e) {
+    console.log("Error running the server." + JSON.stringify(e))
+}
